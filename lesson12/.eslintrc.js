d45ac01d49e2e427ca9ca6bc8854458560422607
Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>
2024-03-24 19:33:13 +0300

    Update .eslintrc.js

--- lesson12/.eslintrc.js
+++ lesson12/.eslintrc.js
@@ -11,7 +11,7 @@ module.exports = {
         node: true,
       },
       files: [
-        '.eslintrc.{js,cjs}',
+        '.eslintrc.{ts}',
       ],
       parserOptions: {
         sourceType: 'script',
