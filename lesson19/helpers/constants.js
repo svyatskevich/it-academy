ad9531ebc06e23e9aa3de571f93b5fcad619d612
Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>
2024-04-12 21:24:16 +0300

    add helpers

---
+++ lesson19/helpers/constants.js
@@ -0,0 +1,11 @@
+const BTTN_NAME = {
+    GITHUB: 'github',
+};
+
+const LINK_NAME = {
+    BLOG: 'blog',
+    SPONSOR: 'docs/sponsor',
+    COMMUNITY: 'community/support',
+};
+
+module.exports = { BTTN_NAME, LINK_NAME };
