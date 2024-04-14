975e2cc7b18c277f01a4990938d96db519f1698e
Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>
2024-04-14 16:03:45 +0300

    add helpers

---
+++ lesson22/helpers/constants.js
@@ -0,0 +1,24 @@
+const BTTN_NAME = {
+    SEARCH: 'search',
+    BAG: 'bag',
+}
+
+const LINK_NAME = {
+    STORE: 'store',
+    MAC: 'mac',
+    IPAD: 'ipad',
+    IPHONE: 'iphone',
+    WATCH:'watch',
+    VISION: 'vision',
+    AIRPODS: 'airpods',
+    TVANDHOME: 'tv-home',
+    ENTERTAINMENT: 'entertainment',
+    ACCESSORIES: 'accessories',
+    SUPPORT: 'support',
+    ORDERS: 'orders',
+    YOURSAVES: 'yoursaves',
+    ACCOUNT: 'account',
+    SIGNIN: 'signin',
+}
+
+module.exports = { BTTN_NAME, LINK_NAME };
