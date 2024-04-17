785463dd2a2a1548000bea3491d465e486cb734e
Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>
2024-04-17 11:40:55 +0300

    fix wdio tests

--- lesson19/test/test.e2e.js
+++ lesson19/test/test.e2e.js
@@ -1,7 +1,7 @@
 const mainPage = require('../pageobjects/mainPage');
 const navBarItems = require('../pageobjects/components/navBarItems');
 const navBarItemsRight = require('../pageobjects/components/navBarItemsRight');
-const { BTTN_NAME, LINK_NAME } = require('../helpers/constants');
+const { LINK_NAME, PAGE_LINK } = require('../helpers/constants');
 
 describe('WebdriverIO e2e tests', () => {
     beforeEach(async () => {
@@ -9,35 +9,30 @@ describe('WebdriverIO e2e tests', () => {
     });
 
     it('Should contain title "Blog" on Blog page', async () => {
-        await mainPage.navigate('https://webdriver.io/');
-        await navBarItems.navigateToPageNavBar(LINK_NAME.BLOG);
+        await navBarItems.navigateToNavBarPage(PAGE_LINK.BLOG);
         await expect(browser).toHaveTitleContaining('Blog');
     });
 
     it('Should contain text "Need Help?" in header of Community page', async () => {
-        await mainPage.navigate('https://webdriver.io/');
-        await navBarItems.navigateToPageNavBar(LINK_NAME.COMMUNITY);
+        await navBarItems.navigateToNavBarPage(PAGE_LINK.COMMUNITY);
         await expect(navBarItems.mainHeader).toHaveText('Need Help?');
     });
 
     it('Should contain "javascript" in URL of 1st search result for "JavaScript"', async () => {
-        await mainPage.navigate('https://webdriver.io/');
         await navBarItemsRight.search('JavaScript');
         await navBarItemsRight.goToFirstSearchResult();
         await expect(browser).toHaveUrlContaining('javascript');
     });
 
     it('Should switch theme and have "dark" theme on Sponsor page', async () => {
-        await mainPage.navigate('https://webdriver.io/');
         await navBarItemsRight.switchModeSite();
         await navBarItemsRight.switchModeSite();
-        await navBarItems.navigateToPageNavBar(LINK_NAME.SPONSOR);
+        await navBarItems.navigateToNavBarPage(PAGE_LINK.SPONSOR);
         await expect(await navBarItemsRight.getTheme()).toEqual('dark');
     });
 
     it('Should go to GitHub and contain repository name "webdriverio"', async () => {
-        await mainPage.navigate('https://webdriver.io/');
-        await navBarItemsRight.goToItemNavBarRight(BTTN_NAME.GITHUB);
+        await navBarItemsRight.goToItemNavBarRight(LINK_NAME.GITHUB);
         await expect(navBarItemsRight.nameRepoGithub).toHaveText('webdriverio');
     });
 });
