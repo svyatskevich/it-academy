be91afab755b4a1e8c3b8169c93b0aae65f68a60
Svetlana Yatskevich <svetlana.yatskevich15@gmail.com>
2024-05-19 01:04:45 +0300

    add apple api tests

---
+++ apple_api/test/bagStatus.test.js
@@ -0,0 +1,34 @@
+const axios = require("axios");
+const { Validator } = require("jsonschema");
+const getUsersJsonSchema = require("../testData/bagStatus.v1.json");
+
+const validator = new Validator();
+
+describe("API Bag Status", function () {
+   let response;
+   let baseUrl = "https://www.apple.com/shop/bag/status";
+   let params = {
+      apikey: "SJHJUH4YFCTTPD4F4",
+   };
+   let headers = {
+      Accept: "*/*",
+      host: "www.apple.com",
+   };
+
+   beforeAll(async () => {
+      response = await axios.get(baseUrl, { params, headers });
+   });
+
+   test("GET /shop/bag/status should return status 200", async () => {
+      expect(response.status).toEqual(200);
+   });
+
+   test("GET /shop/bag/status should be valid jsonschema", async () => {
+      const validationResult = await validator.validate(response.data,getUsersJsonSchema);
+      expect(validationResult.valid).toEqual(true);
+   });
+
+   test(`GET /shop/bag/status should contain 0 items`, async () => {
+      expect(response.data.items).toEqual(0);
+   });
+});
---
+++ apple_api/test/searchItem.test.js
@@ -0,0 +1,36 @@
+const axios = require("axios");
+const { Validator } = require("jsonschema");
+const getUsersJsonSchema = require("../testData/searchItem.v1.json");
+
+const validator = new Validator();
+
+describe("API Search Item", function () {
+   let response;
+   let baseUrl = "https://www.apple.com/shop/searchresults/internalmvc";
+   let params = {
+      find: "airtag",
+      src: "alp",
+      tab: "explore",
+   };
+   let headers = {
+      Accept: "*/*",
+      host: "www.apple.com",
+   };
+
+   beforeAll(async () => {
+      response = await axios.get(baseUrl, { params, headers });
+   });
+
+   test("GET /searchresults/internalmvc should return status 200", async () => {
+      expect(response.status).toEqual(200);
+   });
+
+   test("GET /searchresults/internalmvc should be valid jsonschema", async () => {
+      const validationResult = await validator.validate(response.data,getUsersJsonSchema);
+      expect(validationResult.valid).toEqual(true);
+   });
+
+   test("GET /searchresults/internalmvc should contain search text", async () => {
+      expect(response.data.body.searchTerm).toEqual("airtag");
+   });
+});
